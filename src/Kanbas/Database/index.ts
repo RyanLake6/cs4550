d853da32bf77439a338cbdc46830ce219724b6f6
Ryan Lake <73082037+RyanLake6@users.noreply.github.com>
2024-03-05 13:43:15 -0500

    dashboard add, edit, delete, update

--- src/Kanbas/Database/index.ts
+++ src/Kanbas/Database/index.ts
@@ -2,3 +2,27 @@ import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
 export { courses, modules, assignments };
+
+export type CourseType = {
+  _id: string;
+  name: string;
+  subtitle: string;
+  number: string;
+  startDate: string;
+  endDate: string;
+  image: string;
+};
+
+export interface DB {
+  courses: CourseType[];
+  modules: object;
+  assignments: object;
+}
+
+const db: DB = {
+  courses: courses,
+  modules: modules,
+  assignments: assignments,
+};
+
+export default db;
